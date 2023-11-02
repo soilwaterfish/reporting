e8ae08da732ad09ee598db1905055d53e4c2d8c6
Erickson <12001003029097@FEDIDCARD.GOV>
2023-11-02 16:39:03 -0600

    cleaning

--- R/report_render.R
+++ R/report_render.R
@@ -1,12 +1,13 @@
 #' Run BMP Report
 #'
-#' @param fiscal_year A quoted character.
+#' @param fiscal_year A character for fiscal year of report.
 #' @return A rendered HTML report
 #' @export
 #'
-get_bmp_report <- function(){
+get_bmp_report <- function(fiscal_year){
 
   rmarkdown::render(system.file('app/www', 'nps_accomplishment_reporting.Rmd', package = 'reporting'),
+                    params = fiscal_year,
                     output_format = rmarkdown::word_document(reference_docx = system.file('app/www', 'word_template', package = 'reporting')))
 
   file.copy(from = system.file('app/www', 'nps_accomplishment_reporting.docx', package = 'reporting'),
